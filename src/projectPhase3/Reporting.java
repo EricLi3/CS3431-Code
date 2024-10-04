0846e4993d1597956940ddb52724d38a257abc35
EricLi3 <ericlibeibei@gmail.com>
2024-10-04 19:57:07 -0400

    Fixed error handeling for differnt cli

--- src/projectPhase3/Reporting.java
+++ src/projectPhase3/Reporting.java
@@ -3,64 +3,65 @@ package projectPhase3;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import Data.MyPersonalInfo;
+
 public class Reporting {
     public static void main(String[] args) throws SQLException {
         String userName = null;
         String password = null;
-        if(args.length < 2){
-            System.err.println("USAGE: java Reporting <username> <password> [Option]");
+        if (args.length == 2) {
+            System.err.println("USAGE: java Reporting <username> <password> [Option]\n" +
+                    "1- Report Patients Basic Information\n" +
+                    "2- Report Doctors Basic Information\n" +
+                    "3- Report Admissions Information\n" +
+                    "4- Update Admissions Payment");
             return;
-        }
-        else{
+        } else if (args.length == 3) {
             userName = args[0];
             password = args[1];
-        }
 
-        try {
-            Class.forName("oracle.jdbc.driver.OracleDriver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Where is your Oracle JDBC Driver? Did you follow the execution steps. ");
-            System.out.println("");
-            System.out.println("*****Open the file and read the comments in the beginning of the file****");
-            System.out.println("");
-            e.printStackTrace();
-            return;
-        }
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(
-                    "jdbc:oracle:thin:@oracle.wpi.edu:1521:orcl",
-                    userName,
-                    password);
-            System.out.println("Connection Succes!");
-        } catch (SQLException e) {
-            System.out.println("Connection Failed! Check output console");
-            e.printStackTrace();
-            return;
-        }
 
-        // Now we have the program options
-        if(Integer.parseInt(args[2]) == 1){
-            //Reporting Patients Basic Info.
+            try {
+                Class.forName("oracle.jdbc.driver.OracleDriver");
+            } catch (ClassNotFoundException e) {
+                System.out.println("Where is your Oracle JDBC Driver? Did you follow the execution steps. ");
+                System.out.println("");
+                System.out.println("*****Open the file and read the comments in the beginning of the file****");
+                System.out.println("");
+                e.printStackTrace();
+                return;
+            }
+            Connection connection = null;
+            try {
+                connection = DriverManager.getConnection(
+                        "jdbc:oracle:thin:@oracle.wpi.edu:1521:orcl",
+                        userName,
+                        password);
+                System.out.println("Connection Succes!");
+            } catch (SQLException e) {
+                System.out.println("Connection Failed! Check output console");
+                e.printStackTrace();
+                return;
+            }
 
-        }
-        else if(Integer.parseInt(args[2]) == 2){
-            //Reporting Doctors Basic Info
+            // Now we have the program options
+            if (Integer.parseInt(args[2]) == 1) {
+                //Reporting Patients Basic Info.
+                System.out.println("Hello !!!!");
+            } else if (Integer.parseInt(args[2]) == 2) {
+                //Reporting Doctors Basic Info
 
-        }
-        else if(Integer.parseInt(args[2]) == 3){
-            //Reporting Admissions Info
+            } else if (Integer.parseInt(args[2]) == 3) {
+                //Reporting Admissions Info
 
-        }
-        else if(Integer.parseInt(args[2]) == 4){
-            //Updating Admission Payment
 
-        }
-        else{
-            System.err.println("Options must be of [1, 2, 3, 4]");
-        }
+            } else if (Integer.parseInt(args[2]) == 4) {
+                //Updating Admission Payment
 
-        connection.close();
+            } else {
+                System.err.println("Options must be of [1, 2, 3, 4]");
+            }
+
+            connection.close();
+        }
     }
 }
